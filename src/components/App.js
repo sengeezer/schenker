abbd79b6050a8e7fa4eec4bafdba5389b29ab935
Fabio Albertin <albertin@gmail.com>
2020-09-16 21:01:22 +0100

    Relocated App and added test wizard example

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './company-logo.svg';
+import EnhancedForm from './Wizard';
+import logo from '../company-logo.svg';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <EnhancedForm />
     </div>
   );
 }
